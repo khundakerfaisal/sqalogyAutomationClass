dac1feb9df89ee72c4576117d27e00c1a9696910
khundakerfaisal <khundakermis@gmail.com>
2025-07-10 19:43:27 +0600

    browser added in automation projrct

--- src/test/java/sohozLogin.java
+++ src/test/java/sohozLogin.java
@@ -2,17 +2,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Scanner;
 
 public class sohozLogin {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://www.shohoz.com/contact-us");
-        driver.manage().window().maximize();
-        WebElement bus=driver.findElement(By.id("purpose"));
-        bus.click();
-        Thread.sleep(5000);
+//        WebDriver driver=new ChromeDriver();
+//        driver.get("https://www.shohoz.com/contact-us");
+//        driver.manage().window().maximize();
+//        WebElement bus=driver.findElement(By.id("purpose"));
+//        bus.click();
+//        Thread.sleep(5000);
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://www.guru99.com/");
 
 
     }
