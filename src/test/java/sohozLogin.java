4ea752963fa0bc05375222fda6627d7a4351df1a
khundakerfaisal <khundakermis@gmail.com>
2025-07-13 20:39:51 +0600

    added automation first form

--- src/test/java/sohozLogin.java
+++ src/test/java/sohozLogin.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
 import java.util.Scanner;
 
 public class sohozLogin {
@@ -14,8 +15,21 @@ public class sohozLogin {
 //        WebElement bus=driver.findElement(By.id("purpose"));
 //        bus.click();
 //        Thread.sleep(5000);
-        WebDriver driver=new FirefoxDriver();
-        driver.get("https://www.guru99.com/");
+//        WebDriver driver=new FirefoxDriver();
+//        driver.get("https://www.guru99.com/");
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.findElement(By.id("userName")).sendKeys("Faisal");
+        driver.findElement(By.id("userEmail")).sendKeys("a@yopmail.com");
+        driver.findElement(By.id("currentAddress")).sendKeys("Dhaka");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Dhaka");
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(5000);
+
+
 
 
     }
