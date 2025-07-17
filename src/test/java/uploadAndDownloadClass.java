284bbb9a5aa3a1a6435dc6e08d1bb4d4f77e15ee
khundakerfaisal <khundakermis@gmail.com>
2025-07-17 14:02:49 +0600

    upload with associative path

--- src/test/java/uploadAndDownloadClass.java
+++ src/test/java/uploadAndDownloadClass.java
@@ -13,14 +13,20 @@ public class uploadAndDownloadClass {
         Thread.sleep(500);
 //        srcFileUpload.sendKeys("H:\\Desktop\\1752385269986.jpg");     //Upload from directory
 
-        String filePath="\\src\\test\\resources\\1752385269986.jpg";    //Upload from project root directory
-        String uploadData=System.getProperty("user.dir")+filePath;      //Create  Absolute path
-        System.out.println(uploadData);
-        srcFileUpload.sendKeys(uploadData);
+//        String filePath="\\src\\test\\resources\\1752385269986.jpg";    //Upload from project root directory
+//        String uploadData=System.getProperty("user.dir")+filePath;      //Create  Absolute path
+//        System.out.println(uploadData);
+//        srcFileUpload.sendKeys(uploadData);
+//        Thread.sleep(500);
+//        WebElement clickDownloadButton=driver.findElement(By.id("downloadButton"));
+//        clickDownloadButton.click();
+//        Thread.sleep(500);
+        String filePath="\\src\\test\\resources\\1752385269986.jpg";
+        String uploadFile=System.getProperty("user.dir")+filePath;
+        srcFileUpload.sendKeys(uploadFile);
         Thread.sleep(500);
-        WebElement clickDownloadButton=driver.findElement(By.id("downloadButton"));
-        clickDownloadButton.click();
-        Thread.sleep(500);
+
+
 
     }
 }
