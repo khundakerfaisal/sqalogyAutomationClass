c90984b270d478be32aea95afc2c882faf48fd2c
khundakerfaisal <khundakermis@gmail.com>
2025-07-20 20:28:51 +0600

    added browser tab

---
+++ src/test/java/multipleBrowserTab.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+
+public class multipleBrowserTab {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        //Windows scrolling
+        driver.get("https://demoqa.com/browser-windows");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("tabButton")).click();
+        ArrayList<String> arrayList=new ArrayList<>(driver.getWindowHandles());
+
+        driver.switchTo().window(arrayList.get(1));
+        Thread.sleep(2000);
+        driver.close();
+
+    }
+}
