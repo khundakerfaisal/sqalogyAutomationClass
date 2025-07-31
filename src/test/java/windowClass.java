98aba614a9b1f29c6e978e94ee5c0ffabfbf63f0
khundakerfaisal <khundakermis@gmail.com>
2025-07-31 16:23:44 +0600

    added new window handle

--- src/test/java/windowClass.java
+++ src/test/java/windowClass.java
@@ -1,38 +1,47 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Set;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class windowClass {
     WebDriver driver;
+
     @BeforeAll
     public void setup() {
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 
     }
 
     @Test
-    public void tabHandleClass() throws InterruptedException {
+    public void doWindowHandle() throws InterruptedException {
         driver.get("https://demoqa.com/browser-windows");
-        driver.findElement(By.id("tabButton")).click();
-        Thread.sleep(1000);
-        ArrayList<String> allTab=new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(allTab.get(1));
+        driver.findElement(By.id("windowButton")).click();
+
+        String mainWindow = driver.getWindowHandle();
+        Set<String> allWindow = driver.getWindowHandles();
+        for (String getAllWindow : allWindow) {
+            if (!getAllWindow.equals(mainWindow)) {
+                driver.switchTo().window(getAllWindow);
+                String textActual = driver.findElement(By.id("sampleHeading")).getText();
+                Assertions.assertTrue(textActual.contains("This is a sample page"));
+                break;
+            }
+        }
+
         driver.close();
+        driver.switchTo().window(mainWindow);
 
     }
 
     @AfterAll
     public void close() {
-        driver.close();
+        driver.quit();
     }
 }
