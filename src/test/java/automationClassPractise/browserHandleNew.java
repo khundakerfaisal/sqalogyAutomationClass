565b867eccb4b3983cee4fe512b2ff49a067b5f5
khundakerfaisal <khundakermis@gmail.com>
2025-08-04 15:35:35 +0600

    added window handle

--- src/test/java/automationClassPractise/browserHandleNew.java
+++ src/test/java/automationClassPractise/browserHandleNew.java
@@ -1,8 +1,6 @@
 package automationClassPractise;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -10,10 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
-import java.util.ArrayList;
+import java.util.Set;
 
 
-public class browserTabHandleNew {
+public class browserHandleNew {
 
     public static void main(String[] args) throws InterruptedException {
         ChromeOptions options=new ChromeOptions();
@@ -22,13 +20,22 @@ public class browserTabHandleNew {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
         driver.get("https://demoqa.com/browser-windows");
-        driver.findElement(By.id("tabButton")).click();
-        ArrayList<String> getWindowHandleTab=new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(getWindowHandleTab.get(1));
-        String ExpectedResult=driver.findElement(By.id("sampleHeading")).getText();
-        String ActualResult="This is a sample page";
-        Assertions.assertTrue(ExpectedResult.contains(ActualResult));
-        Thread.sleep(2000);
+        driver.findElement(By.id("windowButton")).click();
+        String mainWindow=driver.getWindowHandle();
+        Set<String> getAllWindow=driver.getWindowHandles();
+        for (String getAllChildWindow:getAllWindow){
+            if (!getAllChildWindow.equals(mainWindow)){
+                driver.switchTo().window(getAllChildWindow);
+                String ExpectedResult=driver.findElement(By.id("sampleHeading")).getText();
+                String ActualResult="This is a sample page";
+                Assertions.assertTrue(ExpectedResult.contains(ActualResult));
+                Thread.sleep(2000);
+
+            }
+
+        }
         driver.close();
+        driver.switchTo().window(mainWindow);
+
     }
 }
