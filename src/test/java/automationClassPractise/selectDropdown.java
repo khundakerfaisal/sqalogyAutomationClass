7efa3637db1a92b3b54e2ae18f688581f65068fc
khundakerfaisal <khundakermis@gmail.com>
2025-08-04 15:50:02 +0600

    added select dropdown class

---
+++ src/test/java/automationClassPractise/selectDropdown.java
@@ -0,0 +1,29 @@
+package automationClassPractise;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class selectDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://demoqa.com/select-menu");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,50)");
+
+        Select select=new Select(driver.findElement(By.id("oldSelectMenu")));
+        select.selectByVisibleText("Blue");
+        Thread.sleep(2000);
+
+
+    }
+}
