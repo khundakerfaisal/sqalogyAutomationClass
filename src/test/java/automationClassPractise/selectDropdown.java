b30eb06251f30be14fd1a73ec732fd95a87ca8f5
khundakerfaisal <khundakermis@gmail.com>
2025-10-26 20:15:02 +0600

    added login id

--- src/test/java/automationClassPractise/selectDropdown.java
+++ src/test/java/automationClassPractise/selectDropdown.java
@@ -1,6 +1,6 @@
 package automationClassPractise;
 
-import org.jspecify.annotations.Nullable;
+//import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
