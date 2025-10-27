f632440a997c9f9f49a3c0c734515bcbb86c0f05
khundakerfaisal <khundakermis@gmail.com>
2025-10-27 19:47:23 +0600

    added interfaces

---
+++ src/main/java/OOPClass/MyInterface.java
@@ -0,0 +1,40 @@
+package OOPClass;
+
+public class MyInterface {
+    public static void main(String[] args) {
+        Animal cat=new Cat();
+        cat.sound();
+        cat.walk();
+        Food food=new Cat();
+        food.eat();
+
+
+
+
+    }
+
+}
+interface Animal{
+
+    void sound();
+    void walk();
+
+}
+interface Food{
+    void eat();
+}
+
+class Cat implements Animal,Food{
+    public void eat(){
+        System.out.println("Cate eat fish");
+    }
+    public void sound(){
+        System.out.println("Cate sound Maww ");
+    }
+    public void walk(){
+        System.out.println("Cate walk");
+    }
+
+
+
+}
